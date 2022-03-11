094beb62330b913c16f4e275d700a8f8c1c8c073
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-03-11 10:46:54 -0500

    feat:main file
    This is the main file to execute the app

---
+++ script.py
@@ -0,0 +1,14 @@
+from logging import raiseExceptions
+
+
+def sum(a, b):  
+    return a + b
+
+
+def main():
+    res = sum(5, 5)
+    print(f'The sum is {res}')
+    
+
+if __name__ == "__main__":
+    main()
