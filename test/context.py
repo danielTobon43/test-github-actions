afd8043abdc760cb2d4c4d86ae851a7e9fe1130d
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-03-12 14:31:14 -0500

    squash! test:python-app workflow

---
+++ test/context.py
@@ -0,0 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.join('..', 'src'))))
+# print(sys.path)
+print(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.join('..', 'src'))))
+import script
--- test/test_script.py
+++ test/test_script.py
@@ -1,5 +1,8 @@
+from .context import script
 from script import sum
 
 
 def test_sum():
     assert sum(3, 2) == 5
+    assert sum(-3, 3) == 0
+    assert sum(-5, -5) == -10
